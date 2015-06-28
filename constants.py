6d36742e929cef98355e76c2ee3713775d8a4d9a
Cody Allen <clallen@purdue.edu>
2015-06-28 17:42:00 +0000

    Added constants

---
+++ constants.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+# SPI Max Block Size
+SPI_MAXBLOCKSIZE = 4096
+
+# Music buffer size
+SPI_BUFFERSIZE = 4
+
+# GPIO pin for bufferfull
+GPIO_BUFFERFULL = 11
+
+# Wireless driver name
+WIRELESS = 'wlan0'
+
+# Voting homepage
+VOTING_DEV = 'index.php'
+VOTING_LIVE = '/var/www/index.php'
